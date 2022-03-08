f1a0613e9974c1a45a38eee27e8796e2fcdb792c
thearthurroth <roth_arthur@outlook.de>
2022-03-08 13:13:50 +0100

    stuff

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import LoginMask from '../components/LoginMask'
+import './Login.css'
+import Container from '@material-ui/core/Container';
 
 const Login = () => {
   return (
-    <div>
+    <div className='Login'>
         <LoginMask/>
     </div>
   )
